33014f9d891d4f3b59ec093ba1267fccfaab7a5f
cfc-droid <cfchoqui@gmail.com>
2025-12-07 16:57:16 -0300

    Update domain_lock_lite.js

--- frontend/js/domain_lock_lite.js
+++ frontend/js/domain_lock_lite.js
@@ -17,7 +17,7 @@
     // (El usuario luego ajusta estos valores)
     // -----------------------------------------
 const ALLOWED_HOSTS = [
-    "xxx-bloqueo-test.com"
+    "campus-cfc-lite-v41-main-demo.pages.dev"
 ];
 
     // -----------------------------------------
