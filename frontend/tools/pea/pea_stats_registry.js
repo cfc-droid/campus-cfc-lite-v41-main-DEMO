6f60230e5a5bcc39e43d8bb359eac891c7b131f9
cfc-droid <cfchoqui@gmail.com>
2026-01-01 18:57:24 -0300

    Update pea_stats_registry.js

--- frontend/tools/pea/pea_stats_registry.js
+++ frontend/tools/pea/pea_stats_registry.js
@@ -57,7 +57,9 @@ const PEA_STATS_RENDERERS = [
 "renderStat_11_top_interferencias_por_resultado",
 "renderStat_12_intensidad_por_resultado",
 "renderStat_13_salud_del_dato",
-"renderStat_14_momentos_estructurales_cambios",   
+"renderStat_14_momentos_estructurales_cambios",
+'renderStat_15_acciones_criticas_por_resultado',
+   
 
    
 ];
